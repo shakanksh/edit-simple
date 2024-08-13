685ea52f21dab12007f563879a8683639407cc3f
shakanksh <shakankshs@gmail.com>
2024-08-13 18:11:36 +0530

    Project Initialised

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,39 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      neutral: {
+        50: "#fafafa",
+        100: "#f5f5f5",
+        200: "#e5e5e5",
+        300: "#d4d4d4",
+        400: "#a3a3a3",
+        500: "#737373",
+        600: "#525252",
+        700: "#404040",
+        800: "#262626",
+        900: "#171717",
+        950: "#0a0a0a",
+      },
+      accent: {
+        "50": "#fff6ec",
+        "100": "#ffead3",
+        "200": "#ffd1a5",
+        "300": "#ffb06d",
+        "400": "#ff8232",
+        "500": "#ff5f0a",
+        "600": "#ff4500",
+        "700": "#cc2f02",
+        "800": "#a1250b",
+        "900": "#82220c",
+        "950": "#460d04",
+      },
+    },
+
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
