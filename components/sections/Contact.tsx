429fcacad9b78c763351dbb814dd6f86da2a4edf
Shakanksh Sinha <shakankshs@gmail.com>
2024-08-17 21:53:16 +0530

    Contact Section Done

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -1,9 +1,27 @@
-export default function Contact() {
+"use client";
+
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+export default function Calendar() {
+	useEffect(() => {
+		(async function () {
+			const cal = await getCalApi({ namespace: "introduction" });
+			cal("ui", {
+				theme: "light",
+				styles: { branding: { brandColor: "#ff4500" } },
+				hideEventTypeDetails: false,
+				layout: "month_view",
+			});
+		})();
+	}, []);
 	return (
-		<section id='contact'>
-			<div>
-				<p>This is Contact section</p>
-			</div>
+		<section id='contact' className='bg-accent-600 py-10'>
+			<Cal
+				namespace='introduction'
+				calLink='edit-simple/introduction'
+				style={{ width: "100%", height: "100%", overflow: "scroll" }}
+				config={{ layout: "month_view" }}
+			/>
 		</section>
 	);
 }
