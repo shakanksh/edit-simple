c80a82888f785333cf49ca6863b6f9f6c1857972
Shakanksh Sinha <shakankshs@gmail.com>
2024-08-22 20:12:26 +0530

    Stats Section and Testimonials Section Revised

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -44,13 +44,13 @@ const services = [
 export default function Services() {
 	return (
 		<section id='services' className='section flex flex-col items-end'>
-			{services.map((services) => (
-				<div className='w-3/5'>
+			{services.map((services, key) => (
+				<div key={key} className='w-3/5'>
 					<div className='flex gap-10 items-center'>
 						<img src={services.icon} alt={services.alt} className='size-16' />
-						<h2 className='text-6xl tracking-normal'>{services.name}</h2>
+						<h2 className='text-5xl tracking-normal'>{services.name}</h2>
 					</div>
-					<div className='h-[1px] bg-accent-600 w-full my-6' />
+					<div className='h-[1px] bg-accent-600 w-full my-4' />
 				</div>
 			))}
 		</section>
--- components/sections/Stats.tsx
+++ components/sections/Stats.tsx
@@ -1,14 +1,41 @@
+const stats = [
+	{
+		number: "500+",
+		name: "Videos Edited",
+	},
+	{
+		number: "3M+",
+		name: "Views Generated",
+	},
+	{
+		number: "40+",
+		name: "Satisfied Clients",
+	},
+	{
+		number: "50+",
+		name: "YT Channels Managed",
+	},
+	{
+		number: "50+",
+		name: "Reels Created",
+	},
+	{
+		number: "4.8/5",
+		name: "Average Client Rating",
+	},
+];
+
 export default function Stats() {
 	return (
 		<section id='stats' className='section flex flex-col gap-32'>
 			<div className='grid grid-cols-12 gap-8'>
-				<p className='col-start-5 col-end-13 text-xl font-medium'>
+				<p className='col-start-1 col-end-7 text-xl font-medium'>
 					Our team boasts a impressive history of delivering high-impact
 					results, leveraging their expertise to build lasting relationships and
 					drive meaningful engagement.
 				</p>
 			</div>
-			<div className='grid grid-cols-12 gap-x-8'>
+			{/* <div className='grid grid-cols-12 gap-x-8'>
 				<div className='col-start-5 col-end-13 text-7xl flex flex-col gap-20'>
 					<div className='stats-wrapper'>
 						<h3 className='stats-number'>500+</h3>
@@ -35,6 +62,17 @@ export default function Stats() {
 						<h3>Average Rating</h3>
 					</div>
 				</div>
+			</div> */}
+			<div className='w-full self-end'>
+				{stats.map((stats, key) => (
+					<div key={key} className='w-2/4'>
+						<div className='flex gap-10 items-center'>
+							<h2 className='text-5xl tracking-normal w-24'>{stats.number}</h2>
+							<h2 className='text-5xl tracking-normal'>{stats.name}</h2>
+						</div>
+						<div className='h-[1px] bg-accent-600 w-full my-4' />
+					</div>
+				))}
 			</div>
 		</section>
 	);
