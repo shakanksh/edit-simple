4f4b681b5c8eba53f376982052d45aa2f623cfd7
Shakanksh Sinha <shakankshs@gmail.com>
2024-08-22 14:31:16 +0530

    Services Section Done

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -1,17 +1,58 @@
+const services = [
+	{
+		name: "YouTube Automation",
+		icon: "/services/yt.svg",
+		alt: "YouTube Logo",
+	},
+	{
+		name: "YouTube Shorts",
+		icon: "/services/yt-shorts.svg",
+		alt: "YouTube Shorts Logo",
+	},
+	{
+		name: "Instagram Reels",
+		icon: "/services/ig-reels.svg",
+		alt: "Instagram Reels Logo",
+	},
+	{
+		name: "TikTok Videos",
+		icon: "/services/tiktok.svg",
+		alt: "TikTok Logo",
+	},
+	{
+		name: "Video Explainers",
+		icon: "/services/video-explainers.svg",
+		alt: "Video Explainers Icon",
+	},
+	{
+		name: "Podcast Editing",
+		icon: "/services/podcast.svg",
+		alt: "Podcast Icon",
+	},
+	{
+		name: "Green Screen Editing",
+		icon: "/services/green-screen.svg",
+		alt: "Green Screen Icon",
+	},
+	{
+		name: "Motion Graphics",
+		icon: "/services/motion-graphics.svg",
+		alt: "Motion Graphics Icon",
+	},
+];
+
 export default function Services() {
-  return (
-    <section id="services" className="section h-[80vh] grid grid-rows-3 p-8 gap-4">
-      <div className="grid grid-cols-6 gap-4">
-        <div className="bg-accent-600 text-accent-100 rounded-2xl">This is Services section</div>
-        <div className="bg-accent-600 text-accent-100 col-span-2 rounded-2xl">This is Services section</div>
- <div className="bg-accent-600 text-accent-100 col-span-3 rounded-2xl">This is Services section</div>
-      </div>
-      <div>
-        <p>This is Services section</p>
-      </div>
-      <div>
-        <p>This is Services section</p>
-      </div>
-    </section>
-  );
+	return (
+		<section id='services' className='section flex flex-col items-end'>
+			{services.map((services) => (
+				<div className='w-3/5'>
+					<div className='flex gap-10 items-center'>
+						<img src={services.icon} alt={services.alt} className='size-16' />
+						<h2 className='text-6xl tracking-normal'>{services.name}</h2>
+					</div>
+					<div className='h-[1px] bg-accent-600 w-full my-6' />
+				</div>
+			))}
+		</section>
+	);
 }
