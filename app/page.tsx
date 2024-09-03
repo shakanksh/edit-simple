71e782d6d605e0c158878849a51ada8fec85d84b
shakanksh <shakankshs@gmail.com>
2024-09-03 20:08:36 +0530

    Some Changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,32 @@ import Testimonials from "@/components/sections/Testimonials";
 import Work from "@/components/sections/Work";
 
 export default function Home() {
-	return (
-		<main className='flex flex-col min-h-screen font-sans bg-accent-100 text-accent-600'>
-			<Navbar />
-			<Hero />
-			<About />
-			<Work />
-			<Testimonials name="Anna D Silva">Working with Edit Simple has been a pleasure and is something that I would strongly recommend. Their work is of the highest quality, are always willing to make any required revisions, and they always complete the task on time.</Testimonials>
-			<Services />
-			<Testimonials name="Samira Ardalan">Edit Simple has been awesome for my reels. Their edits really helped my content stand out, and it’s been great seeing my views go up into the millions. They’re easy to work with and really know their stuff.</Testimonials>
-			<Stats />
-			<Testimonials name="Justin">Edit Simple’s quality is a clear step up from my previous video editor. The final product is more polished and engaging, and it shows in the increased views. The team is professional and easy to work with.</Testimonials>
-			<Contact />
-			<Footer />
-		</main>
-	);
+  return (
+    <main className="flex flex-col min-h-screen font-sans bg-accent-100 text-accent-600">
+      <Navbar />
+      <Hero />
+      <About />
+      <Work />
+      <Testimonials name="Anna D Silva">
+        Working with Edit Simple has been a pleasure and is something that I
+        would strongly recommend. Their work is of the highest quality, are
+        always willing to make any required revisions, and they always complete
+        the task on time.
+      </Testimonials>
+      <Services />
+      <Testimonials name="Samira Ardalan">
+        Edit Simple has been awesome for my reels. Their edits really helped my
+        content stand out, and it’s been great seeing my views go up into the
+        millions. They’re easy to work with and really know their stuff.
+      </Testimonials>
+      <Stats />
+      <Testimonials name="Justin">
+        Edit Simple’s quality is a clear step up from my previous video editor.
+        The final product is more polished and engaging, and it shows in the
+        increased views. The team is professional and easy to work with.
+      </Testimonials>
+      <Contact />
+      <Footer />
+    </main>
+  );
 }
--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -4,7 +4,7 @@ export default function Services() {
       <div className="grid grid-cols-6 gap-4">
         <div className="bg-accent-600 text-accent-100 rounded-2xl">This is Services section</div>
         <div className="bg-accent-600 text-accent-100 col-span-2 rounded-2xl">This is Services section</div>
- <div className="bg-accent-600 text-accent-100 col-span-3 rounded-2xl">This is Services section</div>
+		<div className="bg-accent-600 text-accent-100 col-span-3 rounded-2xl">This is Services section</div>
       </div>
       <div>
         <p>This is Services section</p>
